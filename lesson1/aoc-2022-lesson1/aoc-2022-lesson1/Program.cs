62face03395516124af404523d33f55677749b7d
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-12-18 13:15:44 +0100

    Lesson 1: implemented with C#

---
+++ lesson1/aoc-2022-lesson1/aoc-2022-lesson1/Program.cs
@@ -0,0 +1,52 @@
+﻿LessonA();
+LessonB();
+
+void LessonA()
+{
+    List<int> elves = new List<int>();
+    int currentElf = 0;
+    string parsedText = File.ReadAllText(@"../../../../../input.txt");
+    foreach (string elfEntries in parsedText.Split("\n\n"))
+    {
+        currentElf = 0;
+        foreach (string entry in elfEntries.Split("\n"))
+        {
+            if (entry == "")
+            {
+                continue;
+            }
+
+            currentElf += Int32.Parse(entry);
+        }
+
+        elves.Add(currentElf);
+    }
+
+    Console.WriteLine(elves.Max());
+}
+
+void LessonB()
+{
+    List<int> elves = new List<int>();
+    int currentElf = 0;
+    string parsedText = File.ReadAllText(@"../../../../../input.txt");
+    foreach (string elfEntries in parsedText.Split("\n\n"))
+    {
+        currentElf = 0;
+        foreach (string entry in elfEntries.Split("\n"))
+        {
+            if (entry == "")
+            {
+                continue;
+            }
+
+            currentElf += Int32.Parse(entry);
+        }
+
+        elves.Add(currentElf);
+    }
+
+    List<int> elvesOrdered = elves.OrderDescending().ToList();
+
+    Console.WriteLine(elvesOrdered[0] + elvesOrdered[1] + elvesOrdered[2]);
+}
